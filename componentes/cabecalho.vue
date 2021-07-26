16de9dad54c7174f14fcbdc2eecf880f2d21ed1e
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-25 23:58:56 -0300

    Mudança de formatações

---
+++ componentes/cabecalho.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+      <div class="cabecalho">
+        <img id="minhaFoto" src="imagens/Eu.png" alt = "Foto de Letícia Garcez" />
+        <h1>Letícia Pegoraro Garcez</h1>
+        <p>Estudante de Engenharia de Computação</p>
+      </div>
+      <nav id="menu">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="capacitacoes.html">Capacitações</a></li>
+          <li>
+            <a href="atividades_profisisonais.html">Atividades Profissionais</a>
+          </li>
+          <li>
+            <a href="atividades_voluntarias.html">Atividades Voluntárias</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
